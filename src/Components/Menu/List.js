77a9f131da1bf8fae8f05d12ad5c5ce5786b5604
amir <amirhossienheydarpoor@gmail.com>
2022-12-12 17:36:38 +0330

    add pictures to food tabs

--- src/Components/Menu/List.js
+++ src/Components/Menu/List.js
@@ -1,16 +1,27 @@
-import pizzaImg from './../../Assets/Images/pizza.jpg';
 
 
 export default function List() {
     
     return(
         <div className="grid grid-cols-3 gap-4 m-3 text-white">
-            <button className="col-span-1 rounded-2xl py-4 bg-cover bg-center bg-zinc-900" style={{ backgroundImage: `url(${pizzaImg})` }}>پیتزا</button>
-            <button className="col-span-1 rounded-2xl py-4 bg-slate-700">پیتزا</button>
-            <button className="col-span-1 rounded-2xl py-4 bg-slate-700">پیتزا</button>
-            <button className="col-span-1 rounded-2xl py-4 bg-slate-700">پیتزا</button>
-            <button className="col-span-1 rounded-2xl py-4 bg-slate-700">پیتزا</button>
-            <button className="col-span-1 rounded-2xl py-4 bg-slate-700">پیتزا</button>
+            <div className="rounded-lg bg-cover bg-center  bg-pizza-image">
+                <button className="col-span-1 rounded-lg py-4 w-full bg-gradient-to-r from-[#000000aa] to-[#0000001c]">پیتزا</button>
+            </div>
+            <div className="rounded-lg bg-cover bg-center  bg-sandwich-image">
+                <button className="col-span-1 rounded-lg py-4 w-full bg-gradient-to-r from-[#000000aa] to-[#0000001c]">ساندویچ</button>
+            </div>
+            <div className="rounded-lg bg-cover bg-center  bg-burger-image">
+                <button className="col-span-1 rounded-lg py-4 w-full bg-gradient-to-r from-[#000000aa] to-[#0000001c]">برگر</button>
+            </div>
+            <div className="rounded-lg bg-cover bg-center  bg-fried-image">
+                <button className="col-span-1 rounded-lg py-4 w-full bg-gradient-to-r from-[#000000aa] to-[#0000001c]">سوخاری</button>
+            </div>
+            <div className="rounded-lg bg-cover bg-center  bg-drinks-image">
+                <button className="col-span-1 rounded-lg py-4 w-full bg-gradient-to-r from-[#000000aa] to-[#0000001c]">نوشیدنی</button>
+            </div>
+            <div className="rounded-lg bg-cover bg-center  bg-apetizer-image">
+                <button className="col-span-1 rounded-lg py-4 w-full bg-gradient-to-r from-[#000000aa] to-[#0000001c]">پیش غذا</button>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pizza-image': "url('./Assets/Images/pizza.jpg')",
+        'burger-image': "url('./Assets/Images/burger.jpg')",
+        'sandwich-image': "url('./Assets/Images/sandwich.jpg')",
+        'fried-image': "url('./Assets/Images/fried-2.jpeg')",
+        'drinks-image': "url('./Assets/Images/drinks-2.jpg')",
+        'apetizer-image': "url('./Assets/Images/apetizer-2.jpg')",
+      }
+    },
   },
   plugins: [],
 }
