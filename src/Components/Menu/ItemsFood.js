391e6d19d8d1bdbf6b1f9e29301f2d412b54f025
amir <amirhossienheydarpoor@gmail.com>
2022-12-15 16:41:44 +0330

    add changes to card to have better animtion

--- src/Components/Menu/ItemsFood.js
+++ src/Components/Menu/ItemsFood.js
@@ -16,22 +16,24 @@ export default function ItemsFood() {
 
     let rotate = e => setState(! state)
     return(
-        <div className="card bg-white text-black rounded-[15px] text-center mb-5 border-2 mt-[6rem]">
-                    <div className={`card-header w-full relative transform-style h-[9rem] flex justify-center  ${state ? ' flip' : 'reverse-flip'}`} onClick={rotate}>
-                        <div className={`card-header-front absolute transform-style w-[95%] h-[14.5rem] bottom-[-1rem] backface bg-center bg-cover bg-coketel-image rounded-[15px] `}>
-                        </div>
-                        <div className={`card-header-back absolute transform-style rotate-x w-[95%] h-[14.5rem] backface bg-slate-400 rounded-[15px]`}>
-                            <h5 className="text-black mt-7 text-4xl">Farahzad</h5>
-                        </div>
-                    </div>
-                    <div className="card-body p-5 rounded-[15px] pt-8">
-                        <div className="flex flex-wrap justify-end">
-                            <h2 className="text-2xl">کوکتل گوشت</h2>
-                        </div>
-                    </div>
-                    <div className="card-footer">
-                            <div className="text-3xl rounded-b-md w-full pb-3 px-5 text-gray-800 shadow-md flex justify-start">48,000 T</div>
+        <div className="card relative mt-[6rem] mb-[10rem]">
+            <div className={`card-header w-full relative transform-style h-[9rem] flex justify-center  ${state ? ' flip' : 'reverse-flip'}`} onClick={rotate}>
+                <div className={`card-header-front absolute transform-style w-[95%] h-[14.5rem] bottom-[-1rem] backface bg-center bg-cover bg-burger-image rounded-[15px] `}>
+                </div>
+                <div className={`card-header-back absolute transform-style rotate-x w-[95%] h-[14.5rem] backface bg-slate-400 rounded-[15px]`}>
+                    <h5 className="text-black mt-7 text-4xl">Farahzad</h5>
+                </div>
+            </div>
+            <div className="card-main w-full z-10 absolute top-0 bg-white text-black rounded-[15px] text-center border-2 "> 
+                <div className="card-body p-5 rounded-[15px] pt-[10rem]">
+                    <div className="flex flex-wrap justify-end">
+                        <h2 className="text-2xl">کوکتل گوشت</h2>
                     </div>
                 </div>
+                <div className="card-footer">
+                        <div className="text-3xl rounded-b-md w-full pb-3 px-5 text-gray-800 shadow-md flex justify-start">48,000 T</div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -7,7 +7,7 @@ export default function List() {
     return(
         <>
             <TabFoods />
-            <div className="List px-6 py-10 flex flex-col space-y-28 ">
+            <div className="List px-6 py-10 flex flex-col">
                 <ItemsFood />
                 <ItemsFood />
             </div>
