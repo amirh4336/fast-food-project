7193a865f8933fc0b40053634f5fa0fb1a826dd5
amir <amirhossienheydarpoor@gmail.com>
2022-12-16 07:26:58 +0330

    fix some error

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -18,8 +18,8 @@ export default function Header() {
             </div>
             <div className="col-span-3 flex justify-end">
                 <button type="button" className="text-xs w-12 h-12 bg-gray-500 text-white p-2 py-1 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 ">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 ">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
             </div>
