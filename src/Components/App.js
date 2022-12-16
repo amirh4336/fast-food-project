62a82152ad5bfdac2e15196326111d94c34fb12d
amir <amirhossienheydarpoor@gmail.com>
2022-12-16 07:22:19 +0330

    add context and reducer to project

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,39 @@
+import {useReducer} from 'react';
+// Context
+import Context from '../Context/Context'
+
+// Reducers
+import Reducer from '../Reducers/Reducer'
+
+
+// Component
 import Header from './Header/Header';
 import Menu from './Menu/Menu';
 import Footer from './Footer/Footer';
 
+
 function App() {
+
+  const [state , dispatch] = useReducer(Reducer , {
+    listBurger : "",
+    listsandwich : "",
+    listPizza : "",
+    listApetizer : "",
+    listDrinks : "",
+    listFried : "",
+  })
+
+
   return (
     <main className="min-h-screen font-['Vazir'] " >
-      <Header />
-      <Menu />
-      <Footer />
+      <Context.Provider  value={{
+        listBurger : state.listBurger,
+        dispatch
+      }}>
+        <Header />
+        <Menu />
+        <Footer />
+      </Context.Provider>
     </main>
   );
 }
