63072e533d61c142258715b38d7d95c8b40e65a4
amir <amirhossienheydarpoor@gmail.com>
2022-12-20 15:00:41 +0330

    add style to items navs and change Link to NavLink

--- src/Components/Menu/TabFoods.js
+++ src/Components/Menu/TabFoods.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 // context
 import Context from '../../Context/Context';
 
@@ -11,39 +11,87 @@ export default function TabFoods() {
     return(
         <div className={`grid grid-cols-6 gap-2 fixed w-full bg-white top-20 z-30 px-3 pt-5 pb-0 text-white text-xl shadow-lg transition-all duration-300  ${ headerState ? 'delay-300' : ' -translate-y-20 delay-100'} `}>
             <div className="rounded-lg flex flex-col">
-                <Link to={{pathname : "/"}} className="rounded-lg border border-slate-100 h-14 w-full flex justify-center content-center">
+                <NavLink to={{pathname : "/"}} className={({ isActive }) => {   
+                                                                                let className = 'rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center';    
+                                                                                if (isActive) {
+                                                                                    className += ' bg-slate-900'
+                                                                                } else {
+                                                                                    className += ''
+                                                                                }
+                                                                                return className
+                                                                            }}>
                     <div className="h-12 w-12 bg-cover bg-center  bg-pizza-image"></div>
-                </Link>
+                </NavLink>
                 <h3 className="text-[.75rem] text-center p-0 m-0 text-black">پیتزا</h3>
             </div>
             <div className="rounded-lg flex flex-col">
-                <Link to={{pathname : "/listSandwich"}} className="rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center">
+                <NavLink to={{pathname : "/listSandwich"}} className={({ isActive }) => {   
+                                                                                let className = 'rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center';    
+                                                                                if (isActive) {
+                                                                                    className += ' bg-slate-900'
+                                                                                } else {
+                                                                                    className += ''
+                                                                                }
+                                                                                return className
+                                                                            }}>
                     <div className="h-12 w-12 bg-cover bg-center  bg-sandwich-image"></div>
-                </Link>
+                </NavLink>
                 <h3 className="text-center text-[.75rem] rounded-lg w-full text-black">ساندویچ</h3>
             </div>
             <div className="rounded-lg flex flex-col">
-                <Link to={{pathname : "/listBurger"}} className="rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center">
+                <NavLink to={{pathname : "/listBurger"}} className={({ isActive }) => {   
+                                                                                let className = 'rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center';    
+                                                                                if (isActive) {
+                                                                                    className += ' bg-slate-900'
+                                                                                } else {
+                                                                                    className += ''
+                                                                                }
+                                                                                return className
+                                                                            }}>
                     <div className="h-12 w-12 bg-cover bg-center  bg-burger-image"></div>
-                </Link>
+                </NavLink>
                 <h3 className="text-center text-[.75rem] rounded-lg w-full text-black">برگر</h3>
             </div>
             <div className="rounded-lg flex flex-col">
-                <Link to={{pathname : "/listFried"}} className="rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center">
+                <NavLink to={{pathname : "/listFried"}} className={({ isActive }) => {   
+                                                                                let className = 'rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center';    
+                                                                                if (isActive) {
+                                                                                    className += ' bg-slate-900'
+                                                                                } else {
+                                                                                    className += ''
+                                                                                }
+                                                                                return className
+                                                                            }}>
                     <div className="h-12 w-12 bg-cover bg-center  bg-fried-image"></div>
-                </Link>
+                </NavLink>
                 <h3 className="text-center text-[.75rem] rounded-lg w-full text-black">سوخاری</h3>
             </div>
             <div className="rounded-lg flex flex-col">
-                <Link to={{pathname : "/listDrink"}} className="rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center">
+                <NavLink to={{pathname : "/listDrink"}} className={({ isActive }) => {   
+                                                                                let className = 'rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center';    
+                                                                                if (isActive) {
+                                                                                    className += ' bg-slate-900'
+                                                                                } else {
+                                                                                    className += ''
+                                                                                }
+                                                                                return className
+                                                                            }}>
                     <div className="h-12 w-12 bg-cover bg-center  bg-drinks-image"></div>
-                </Link>
+                </NavLink>
                 <h3 className="text-center text-[.75rem] rounded-lg w-full text-black">نوشیدنی</h3>
             </div>
             <div className="rounded-lg flex flex-col">
-                <Link to={{pathname : "/listApetizer"}} className="rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center">
+                <NavLink to={{pathname : "/listApetizer"}} className={({ isActive }) => {   
+                                                                                let className = 'rounded-lg border border-slate-100 h-14 w-full flex justify-center items-center';    
+                                                                                if (isActive) {
+                                                                                    className += ' bg-slate-900'
+                                                                                } else {
+                                                                                    className += ''
+                                                                                }
+                                                                                return className
+                                                                            }}>
                     <div className="h-12 w-12 bg-cover bg-center  bg-apetizer-image"></div>
-                </Link>
+                </NavLink>
                 <h3 className="text-center text-[.75rem] rounded-lg w-full text-black">پیش غذا</h3>
             </div>
         </div>
