79a379ee9a1213bee69c7addb5a5ef6740d06e4a
amir <amirhossienheydarpoor@gmail.com>
2022-12-25 13:45:43 +0330

    add state for Tabs.js for toggle better subtabfood

--- src/Components/Menu/TabFoods/TabFood.js
+++ src/Components/Menu/TabFoods/TabFood.js
@@ -1,25 +1,28 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 
 
-export default function TabFood({TabsFood}) {
+// export default function TabFood({TabsFood , ToggleSub}) {
 
-    const {name , bg , path} = TabsFood
+//     const {name , bg , path} = TabsFood ;
+//     const {setShow} = ToggleSub;
     
-    return(
-        <div className="rounded-lg flex flex-col">
-            <NavLink to={{pathname: path}} className={({ isActive }) => {   
-                                                                            let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
-                                                                            if (isActive) {
-                                                                                className += ' bg-[#052130] dark:bg-[#F2F1EE] '
-                                                                            } else {
-                                                                                className += ''
-                                                                            }
-                                                                            return className
-                                                                        }}>
-                <div className={`h-12 w-12 bg-cover bg-center  ${bg}`}></div>
-            </NavLink>
-            <h3 className="text-[.75rem] text-center p-0 m-0">{`${name}`}</h3>
-        </div>
-    )
-}
+    
+
+//     return(
+//         <div className="rounded-lg flex flex-col">
+//             <NavLink to={{pathname: path}} onClick={() => {setShow(false)}} className={({ isActive }) => {   
+//                                                                             let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
+//                                                                             if (isActive) {
+//                                                                                 className += ' bg-[#052130] dark:bg-[#F2F1EE] '
+//                                                                             } else {
+//                                                                                 className += ''
+//                                                                             }
+//                                                                             return className
+//                                                                         }}>
+//                 <div className={`h-12 w-12 bg-cover bg-center  ${bg}`}></div>
+//             </NavLink>
+//             <h3 className="text-[.75rem] text-center p-0 m-0">{`${name}`}</h3>
+//         </div>
+//     )
+// }
--- src/Components/Menu/TabFoods/Tabs.js
+++ src/Components/Menu/TabFoods/Tabs.js
@@ -1,26 +1,27 @@
-import { useContext } from 'react';
+import { useContext , useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 // context
 import Context from '../../../Context/Context';
 
 // Components
-import TabFood from './TabFood';
+// import TabFood from './TabFood';
 import SubTabFood from './SubTab/SubTabFood';
 
 export default function TabFoods() {
 
     const { headerState } = useContext(Context);
 
-    const listTabFoods =  [
-        // { id:1 , name:"پیتزا" , bg:"bg-pizza-image" , path:"/listPizza/listPizzaItaly" },
-        { id:2 , name:"ساندویچ" , bg:"bg-sandwich-image" , path:"/listSandwich" },
-        { id:3 , name:"برگر" , bg:"bg-burger-image" , path:"/" },
-        { id:4 , name:"سوخاری" , bg:"bg-fried-image" , path:"/listFried" },
-        { id:5 , name:"نوشیدنی" , bg:"bg-drinks-image" , path:"/listDrink" },
-        { id:6 , name:"پیش غذا" , bg:"bg-apetizer-image" , path:"/listApetizer" },            
-    ];
+    // const listTabFoods =  [
+    //     // { id:1 , name:"پیتزا" , bg:"bg-pizza-image" , path:"/listPizza/listPizzaItaly" },
+    //     { id:2 , name:"ساندویچ" , bg:"bg-sandwich-image" , path:"/listSandwich" },
+    //     { id:3 , name:"برگر" , bg:"bg-burger-image" , path:"/" },
+    //     { id:4 , name:"سوخاری" , bg:"bg-fried-image" , path:"/listFried" },
+    //     { id:5 , name:"نوشیدنی" , bg:"bg-drinks-image" , path:"/listDrink" },
+    //     { id:6 , name:"پیش غذا" , bg:"bg-apetizer-image" , path:"/listApetizer" },            
+    // ];
     
+    const [Show, setShow] = useState(false)
 
     return(
         <>
@@ -28,7 +29,7 @@ export default function TabFoods() {
                 
                 {/* pizza list */}
                 <div className="rounded-lg flex flex-col">
-                    <NavLink  to={{pathname: "/listPizza/listPizzaItaly"}} className={({ isActive }) => {   
+                    <NavLink  to={{pathname: "/listPizza/listPizzaItaly"}} onClick={() => {setShow(true)}} className={({ isActive }) => {   
                                                                                                     let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
                                                                                                     if (isActive || window.location.pathname === '/listPizza/listPizzaAmerican' ) {
                                                                                                         className += ' bg-[#052130] dark:bg-[#F2F1EE] '
@@ -41,13 +42,86 @@ export default function TabFoods() {
                     </NavLink>
                     <h3 className="text-[.75rem] text-center p-0 m-0">پیتزا</h3>
                 </div>
+                <div className="rounded-lg flex flex-col">
+                    <NavLink  to={{pathname: "/listSandwich"}} onClick={() => {setShow(false)}} className={({ isActive }) => {   
+                                                                                                    let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
+                                                                                                    if ( isActive ) {
+                                                                                                        className += ' bg-[#052130] dark:bg-[#F2F1EE] '
+                                                                                                    } else {
+                                                                                                        className += ''
+                                                                                                    }
+                                                                                                    return className
+                                                                                                }}>
+                        <div className={`h-12 w-12 bg-cover bg-center  bg-sandwich-image`}></div>
+                    </NavLink>
+                    <h3 className="text-[.75rem] text-center p-0 m-0">ساندویچ</h3>
+                </div>
+                <div className="rounded-lg flex flex-col">
+                    <NavLink  to={{pathname: "/"}} onClick={() => {setShow(false)}} className={({ isActive }) => {   
+                                                                                                    let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
+                                                                                                    if ( isActive ) {
+                                                                                                        className += ' bg-[#052130] dark:bg-[#F2F1EE] '
+                                                                                                    } else {
+                                                                                                        className += ''
+                                                                                                    }
+                                                                                                    return className
+                                                                                                }}>
+                        <div className={`h-12 w-12 bg-cover bg-center  bg-burger-image`}></div>
+                    </NavLink>
+                    <h3 className="text-[.75rem] text-center p-0 m-0">برگر</h3>
+                </div>
+                <div className="rounded-lg flex flex-col">
+                    <NavLink  to={{pathname: "/listFried"}} onClick={() => {setShow(false)}} className={({ isActive }) => {   
+                                                                                                    let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
+                                                                                                    if (isActive) {
+                                                                                                        className += ' bg-[#052130] dark:bg-[#F2F1EE] '
+                                                                                                    } else {
+                                                                                                        className += ''
+                                                                                                    }
+                                                                                                    return className
+                                                                                                }}>
+                        <div className={`h-12 w-12 bg-cover bg-center  bg-fried-image`}></div>
+                    </NavLink>
+                    <h3 className="text-[.75rem] text-center p-0 m-0">سوخاری</h3>
+                </div>
+                <div className="rounded-lg flex flex-col">
+                    <NavLink  to={{pathname: "/listDrink"}} onClick={() => {setShow(false)}} className={({ isActive }) => {   
+                                                                                                    let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
+                                                                                                    if (isActive ) {
+                                                                                                        className += ' bg-[#052130] dark:bg-[#F2F1EE] '
+                                                                                                    } else {
+                                                                                                        className += ''
+                                                                                                    }
+                                                                                                    return className
+                                                                                                }}>
+                        <div className={`h-12 w-12 bg-cover bg-center  bg-drinks-image`}></div>
+                    </NavLink>
+                    <h3 className="text-[.75rem] text-center p-0 m-0">نوشیدنی</h3>
+                </div>
+                <div className="rounded-lg flex flex-col">
+                    <NavLink  to={{pathname: "/listApetizer"}} onClick={() => {setShow(false)}} className={({ isActive }) => {   
+                                                                                                    let className = 'rounded-lg border dark:border-slate-700 h-14 w-full flex justify-center items-center';    
+                                                                                                    if (isActive) {
+                                                                                                        className += ' bg-[#052130] dark:bg-[#F2F1EE] '
+                                                                                                    } else {
+                                                                                                        className += ''
+                                                                                                    }
+                                                                                                    return className
+                                                                                                }}>
+                        <div className={`h-12 w-12 bg-cover bg-center  bg-apetizer-image`}></div>
+                    </NavLink>
+                    <h3 className="text-[.75rem] text-center p-0 m-0">پیش غذا</h3>
+                </div>
                 
-                {
-                    listTabFoods.map(itemTabFood => <TabFood  key={itemTabFood.id} TabsFood={itemTabFood} />)
-                }
+                {/* {
+                    listTabFoods.map(itemTabFood => <TabFood  key={itemTabFood.id} TabsFood={itemTabFood} ToggleSub={setShow} />)
+                } */}
 
             </div>
-            <SubTabFood />
+            
+            <div className={`grid grid-cols-2 z-20 p-3 fixed top-[11.5rem] bg-[#F2F1EE] text-[#052130] dark:bg-[#052130] dark:text-[#F2F1EE] w-full text-center transition-all duration-300 ${ headerState ? 'delay-300' : ' -translate-y-20 delay-100'} ${ Show ? '' : '-translate-y-[10rem]'}`}>
+                <SubTabFood />
+            </div>
         </>
 
     )
